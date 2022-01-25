18cba274c2a5824fd6583ec2b6bcef13b4ff19ef
Grant Pierce <glpierce174@gmail.com>
2022-01-25 09:28:25 -0800

    Added image field to books

--- db/migrate/20220124225820_create_books.rb
+++ db/migrate/20220124225820_create_books.rb
@@ -7,6 +7,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
       t.string :genre
       t.float :price
       t.float :rating
+      t.string :image
       t.integer :total_quantity
       t.integer :quantity_in_stock
     end
