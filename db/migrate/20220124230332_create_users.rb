44dbec4f347320ae4473385c3ad466ca1c7d0e88
Grant Pierce <glpierce174@gmail.com>
2022-01-25 09:10:12 -0800

    Enhanced user schema by adding name, email, phone, address

--- db/migrate/20220124230332_create_users.rb
+++ db/migrate/20220124230332_create_users.rb
@@ -3,6 +3,10 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :username
       t.string :password
+      t.string :email
+      t.string :address
+      t.string :name
+      t.integer :phone
     end
   end
 end
